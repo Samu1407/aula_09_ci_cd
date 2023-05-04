f3da61c1253d83a5475ce70b0b70266911a687db
Samu1407 <sccsilva13@gmail.com>
2023-05-04 09:17:55 -0300

    Funcao da calculadora

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,27 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+def test_soma():
+    a = 2
+    b = 4
+
+    output = methods.soma(a, b)
+    assert output == 6
+def test_sub():
+    a = 4
+    b = 2
+
+    output = methods.sub(a, b)
+    assert output == 2
+def test_mult():
+    a = 2
+    b = 4
+
+    output = methods.mult(a, b)
+    assert output == 8
+def test_div():
+    a = 4
+    b = 2
+
+    output = methods.div(a, b)
+    assert output == 2
